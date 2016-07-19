8c1645066379d424a7510776781584d3595eaf94
April Arcus <april.arcus@gmail.com>
2016-07-19 14:48:15 -0700

    pyuv

--- powerline.rb
+++ powerline.rb
@@ -1,1 +1,13 @@
-dep 'powerline-status.pip'
+# dep 'automake.managed'
+dep 'automake.managed'
+dep 'libtool.managed'
+
+dep 'pyuv.pip'  do
+	requires 'automake.managed'
+	requires 'libtool.managed'
+end
+
+
+dep 'powerline-status.pip'  do
+	requires 'pyuv.pip'
+end
