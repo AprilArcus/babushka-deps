14d306ac9debdcaab7623921be7c0d361395ff57
April Arcus <april.arcus@gmail.com>
2016-07-19 15:14:08 -0700

    powerline deps

--- powerline.rb
+++ powerline.rb
@@ -1,18 +1,28 @@
-# dep 'automake.managed'
-dep 'automake.managed'
-dep 'libtool.managed'
+dep 'socat.managed'
+
+dep 'psutil.pip'
+
+dep 'mercurial.pip'
+
 dep 'libgit2.managed'
 dep 'pygit2.pip' do
-	requires 'libgit2.managed'
+  requires 'libgit2.managed'
 end
 
+dep 'bzr.pip'
+
+dep 'automake.managed'
+dep 'libtool.managed'
 dep 'pyuv.pip'  do
-	requires 'automake.managed'
-	requires 'libtool.managed'
+  requires 'automake.managed'
+  requires 'libtool.managed'
 end
-
 
 dep 'powerline-status.pip'  do
-	requires 'pyuv.pip'
-	requires 'pybgit2.pip'
+  requires 'socat.managed'
+  requires 'psutil.pip'
+  requires 'mercurial.pip'
+  requires 'pygit2.pip'
+  requires 'bzr.pip'
+  requires 'pyuv.pip'
 end
